c74d14b9941e87023a35f4b1cc0ffc6b6728ad37
Geoffrey Graddy <geoffrey.graddy@gmail.com>
2022-03-23 13:08:09 -0400

    Initial Commit

---
+++ src/app/home-inventory-form/home-inventory-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-inventory-form',
+  templateUrl: './home-inventory-form.component.html',
+  styleUrls: ['./home-inventory-form.component.css']
+})
+export class HomeInventoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
